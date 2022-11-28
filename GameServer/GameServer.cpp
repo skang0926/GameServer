be9873329d38ebe6fc535f20f7b5a9ae2aa139de
skang0926 <skang0926@naver.com>
2022-11-28 15:17:53 +0900

    # Lock-Base Stack / Queue

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -1,7 +1,47 @@
 ﻿#include "pch.h"
 #include <iostream>
 
+#include <thread>
+#include <atomic>
+#include <mutex>
+#include <Windows.h>
+#include "ConcurrentQueue.h"
+#include "ConcurrentStack.h"
+
+LockQueue<int32> q;
+LockStack<int32> s;
+
+void Push()
+{
+	while (true)
+	{
+		int32 value = rand() % 100;
+		q.Push(value);
+
+		this_thread::sleep_for(10ms);
+	}
+}
+
+void Pop()
+{
+	while (true)
+	{
+		int32 data = 0;
+		if (q.TryPop(data))
+		{
+			cout << data << endl;
+		}
+	}
+}
+
 int main()
 {
-	HelloWorld();
+	thread t1(Push);
+	thread t2(Pop);
+	thread t3(Pop);
+
+	t1.join();
+	t2.join();
+	t2.join();
+	
 }
