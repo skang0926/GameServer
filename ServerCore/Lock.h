4f4ce375fe45a59f719f1644602f92e7a6929509
skang0926 <skang0926@naver.com>
2022-11-30 23:45:27 +0900

    # ReadWrite Lock

---
+++ ServerCore/Lock.h
@@ -0,0 +1,65 @@
+#pragma once
+#include "Types.h"
+
+/***************
+R W SpinLock 
+***************/
+/*
+32bit
+[WWWWWWWW][WWWWWWWW][RRRRRRRR][RRRRRRRR]
+
+W : Write Flag (exclusive lock owner ThreadId)
+R : Read Flag (Shared Lock Count)
+
+W Lock -> W Lock : O
+W Lock -> R Lock : O
+R Lock -> W Lock : X
+
+*/
+
+
+class Lock
+{
+	enum : uint32
+	{
+		ACQUIRE_TIMEOUT_TICK = 10000,
+		MAX_SPIN_COUNT = 5000,
+		WRITE_THREAD_MASK = 0xFFFF'0000,
+		READ_COUNT_MASK = 0x0000'FFFF,
+		EMPTY_FLAG = 0x0000'0000
+	};
+
+public:
+	void WriteLock();
+	void WriteUnlock();
+	void ReadLock();
+	void ReadUnlock();
+
+private:
+	Atomic<uint32> _lockFlag;
+	uint16 _writeCount = 0;
+};
+
+/***************
+LockGuard
+***************/
+
+//RAII
+class ReadLockGuard
+{
+public:
+	ReadLockGuard(Lock& lock) : _lock(lock) { _lock.ReadLock(); }
+	~ReadLockGuard() { _lock.ReadUnlock(); }
+private:
+	Lock& _lock;
+};
+
+class WriteLockGuard
+{
+public:
+	WriteLockGuard(Lock& lock) : _lock(lock) { _lock.WriteLock(); }
+	~WriteLockGuard() { _lock.WriteUnlock(); }
+private:
+	Lock& _lock;
+};
+
