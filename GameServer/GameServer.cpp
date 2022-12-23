a62730a8fcf8e279b8aea2b6cc73963b4c62273d
skang0926 <skang0926@naver.com>
2022-12-23 21:34:43 +0900

    # Lambda JobQueue

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -11,47 +11,9 @@
 #include "Job.h"
 #include "Room.h"
 
-void HealByValue(int64 target, int32 value)
-{
-	cout << target << "한테 힐" << value << "만큼 줌" << endl;
-}
-
-class Knight
-{
-public:
-	void HealMe(int32 value)
-	{
-		cout << "HealMe! " << value << endl;
-	}
-};
-
 int main()
 {
 	GSessionManager = new GameSessionManager;
-
-	auto tup = std::tuple<int32, int32>(1, 2);
-	auto val0 = std::get<0>(tup);
-	auto val1 = std::get<1>(tup);
-
-	auto s = gen_seq<3>();
-	// gen_seq<3>
-	// : gen_seq<2, 2>
-	// : gen_seq<1, 1, 2>
-	// : gen_seq<0, 0, 1, 2>
-	// : seq<0, 1, 2>
-
-	// TEST JOB
-	{
-		FuncJob<void, int64, int32> job(HealByValue, 100, 10);
-		job.Execute();
-	}
-	{
-		Knight k1;
-		MemberJob job2(&k1, &Knight::HealMe, 10);
-		job2.Execute();
-	}
-
-	// JOB
 
 	ClientPacketHandler::Init();
 
@@ -76,7 +38,7 @@ int main()
 
 	while (true)
 	{
-		GRoom.FlushJob();
+		GRoom->FlushJob();
 		this_thread::sleep_for(1ms);
 	}
 
